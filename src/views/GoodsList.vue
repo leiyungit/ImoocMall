e2ba8b618632313284d76d1ec317c07b2ca2f1a6
leiyungit <744561873@qq.com>
2020-03-22 16:33:52 +0800

    first commit

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <nav-bread><span>Goods</span></nav-bread>
+        <div></div>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+import './../assets/css/base.css'
+import './../assets/css/product.css'
+import NavHeader from './../components/NavHeader'
+import NavFooter from './../components/NavFooter'
+import NavBread from './../components/NavBread'
+export default {
+    data(){
+        return{
+            msg:"test"
+        }
+    },
+    components:{
+        NavHeader,
+        NavFooter,
+        NavBread
+    }
+}
+</script>
+<style scoped>
+
+</style>
